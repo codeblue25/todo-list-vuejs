75187ceb6669c483d7e0ac486db8159cad62dcea
codeblue25 <chois1031@gmail.com>
2022-06-20 00:01:33 +0900

    Set basic components

---
+++ todo-list-vuejs/src/components/TodoInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="inputBox shadow">
+    <input 
+      type="text"
+      v-model="todoItem"
+      @keyup.enter="addTodo"
+    />
+    <span class="addContainer"
+      @click="addTodo"
+    ><font-awesome-icon icon="fa-solid fa-plus" />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    todoItem: '',
+  }),
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.todoItem, this.todoItem); 
+      this.clearInput();
+    },
+    clearInput() {
+      this.todoItem = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: #fff;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: #fff;
+  vertical-align: middle;
+}
+</style>
---
+++ todo-list-vuejs/src/components/TodoList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <ul>
+      <li 
+        v-for="(todoItem, index) in this.todoItems" :key="todoItem"
+        class="shadow"
+      >
+        {{ todoItem }}
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <font-awesome-icon icon="fa-solid fa-trash-can" />
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    todoItems: [],
+  }),
+  methods: {
+    removeTodo(todoItem, index) {
+      console.log('bye');
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created() {
+    console.log('hi im just created');
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background-color: #fff;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62ACDE;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #B3ADAD;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #B3ADAD;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #DE4343;
+  cursor: pointer;
+}
+</style>
