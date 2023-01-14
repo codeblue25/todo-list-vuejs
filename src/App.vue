9adbda166ec478ee4f422f6e01c77a2e629b80d9
codeblue25 <chois1031@gmail.com>
2023-01-14 17:09:28 +0900

    Refactor to use Vuex (add todoItems)

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList
+      :propsData="todoItems"
+      @toggleTodoItems="toggleTodoItems"
+      @removeTodoItems="removeTodoItems"
+    ></TodoList>
+    <TodoFooter
+      @clearTodoItems="clearTodoItems"
+    ></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  },
+  data: () => ({
+    todoItems: [],
+  }),
+  methods: {
+    toggleTodoItems(value, index) {
+      // value.completed = !value.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      localStorage.removeItem(value.item);
+      localStorage.setItem(value.item, JSON.stringify(value));
+    },
+    removeTodoItems(value, index) {
+      localStorage.removeItem(value.item);
+      this.todoItems.splice(index, 1);
+    },
+    clearTodoItems() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
